3685b89d9a29234841390e20bd1bde3526905516
Jhonny <jhonnydanieldavila@gmail.com>
2026-05-29 11:16:10 -0400

    ss

--- controller/archivador-controller.php
+++ controller/archivador-controller.php
@@ -1,11 +1,11 @@
 <?php
-require_once('model/archivador-model.php');
+    require_once('model/archivador-model.php');
 
-class ArchivadorController {
-    
-    public function listar() {
-        $model = new ArchivadorModel();
-        return $model->consultar_archivador_model();
+    class ArchivadorController {
+        
+        public function listar() {
+            $model = new ArchivadorModel();
+            return $model->consultar_archivador_model();
+        }
+        
     }
-    
-}
--- model/archivador-model.php
+++ model/archivador-model.php
@@ -1,49 +1,54 @@
 <?php
-require_once('model/conexion.php');
-
-class ArchivadorModel extends Conexion {
+    require_once('model/conexion.php');
     
-    // 1. Propiedades para usar con Setters
-    private $numero;
-    private $descripcion;
-    private $estatus;
+    class ArchivadorModel extends Conexion {
+        
+        // 1. Propiedades para usar con Setters
+        private $numero;
+        private $descripcion;
+        private $estatus;
 
-    // Métodos Setter
-    public function set_Numero($numero) { $this->numero = $numero; }
-    public function set_Descripcion($descripcion) { $this->descripcion = $descripcion; }
-    public function set_Estatus($estatus) { $this->estatus = $estatus; }
+        // 2. MÉTODO PARA REGISTRAR
+        public function registrar_archivador_model() {
+            try {
+                // Consulta SQL corregida para incluir el parámetro :estatus
+                $registro = "INSERT INTO tbl_archivadores (numeroArchivador, descripcionArchivador, estatusArchivador) VALUES (:numero, :descripcion, :estatus)";
+                
+                $strExec = $this->Conexion()->prepare($registro);
+                
+                // Usamos las propiedades de la clase que llenamos con los setters
+                $strExec->bindParam(':numero', $this->numero);
+                $strExec->bindParam(':descripcion', $this->descripcion);
+                $strExec->bindParam(':estatus', $this->estatus);
+                
+                return $strExec->execute();
+            } catch (Exception $e) {
+                    error_log('Error en registrar_archivador_model(): ' . $e->getMessage());
+                    exit();
+            }
+        }
 
-    // 2. MÉTODO PARA REGISTRAR
-    public function registrar_archivador_model() {
-        try {
-            // Consulta SQL corregida para incluir el parámetro :estatus
-            $sql = "INSERT INTO tbl_archivadores (numeroArchivador, descripcionArchivador, estatusArchivador) 
-                    VALUES (:numero, :descripcion, :estatus)";
-            
-            $stmt = $this->Conexion()->prepare($sql);
-            
-            // Usamos las propiedades de la clase que llenamos con los setters
-            $stmt->bindParam(':numero', $this->numero, PDO::PARAM_STR);
-            $stmt->bindParam(':descripcion', $this->descripcion, PDO::PARAM_STR);
-            $stmt->bindParam(':estatus', $this->estatus, PDO::PARAM_STR);
-            
-            return $stmt->execute();
-        } catch (Exception $e) {
-            // Si quieres ver el error real, puedes comentar esto y poner: echo $e->getMessage();
-            return false;
+        // 3. MÉTODO PARA CONSULTAR
+        public function consultar_archivador_model() {
+            try {
+                $registro = "SELECT numeroArchivador, descripcionArchivador, estatusArchivador FROM tbl_archivadores";
+                $strExec = $this->Conexion()->prepare($registro);
+                $strExec->execute();
+                return $strExec->fetchAll(PDO::FETCH_ASSOC);
+            } catch (Exception $e) {
+                return array();
+            }
+        }
+
+        // Métodos Setter
+        public function set_Numero($numero) {
+            $this->numero = $numero; 
+        }
+        public function set_Descripcion($descripcion) {
+            $this->descripcion = $descripcion; 
+        }
+        public function set_Estatus($estatus) { 
+            $this->estatus = $estatus; 
         }
     }
-
-    // 3. MÉTODO PARA CONSULTAR
-    public function consultar_archivador_model() {
-        try {
-            $sql = "SELECT numeroArchivador, descripcionArchivador, estatusArchivador FROM tbl_archivadores";
-            $stmt = $this->Conexion()->prepare($sql);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            return array();
-        }
-    }
-}
 ?>
